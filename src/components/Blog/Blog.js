e575be6fa7c7608adf381d58464af76e6230e8e4
afrojani <sultana.ani2019@gmail.com>
2022-09-30 01:01:11 -0500

    Q&A added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -13,66 +13,38 @@ const Blog = () => {
                     <div className="accordion-item m-3">
                         <h2 className="accordion-header" id="headingOne">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> What are
-                                the differences between Flexbox and Grid?</button>
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> How does React work?</button>
                         </h2>
                         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne"
                             data-bs-parent="#accordionExample">
-                            <div className="accordion-body"> CSS Grid and Flexbox are layout models that share similarities
-                                and can be used together. The key difference is that CSS Grid can be used to create
-                                two-dimensional layouts, while Flexbox can only be used to create one-dimensional
-                                layouts. That means you can place components along the X- and Y-axis in CSS Grid and
-                                only one axis in Flexbox. </div>
+                            <div className="accordion-body">React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes. What does React actually do?
+                                It is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike. </div>
                         </div>
                     </div>
                     <div className="accordion-item m-3">
                         <h2 className="accordion-header" id="headingTwo">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"> What
-                                are the differences between Bootstrap and Tailwind?</button>
+                                are the differences between Props and State?</button>
                         </h2>
                         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo"
                             data-bs-parent="#accordionExample">
-                            <div className="accordion-body"> Tailwind offers predesigned widgets to build a site from
-                                scratch with fast UI development. Bootstrap comes with a set of pre-styled responsive,
-                                mobile-first components that possess a definite UI kit. Tailwind CSS uses a set of
-                                utility classes to create a neat UI with more flexibility and uniqueness. Sites created
-                                using Bootstrap follow the generic pattern that makes them look identical. </div>
+                            <div className="accordion-body">State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props. Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this.setState property is used to update the state values in the component. </div>
                         </div>
                     </div>
                     <div className="accordion-item m-3">
                         <h2 className="accordion-header" id="headingThree">
                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                What is CSS box model? </button>
+                                How 'UseEffect' can be used other than loading data via API? </button>
                         </h2>
                         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree"
                             data-bs-parent="#accordionExample">
-                            <div className="accordion-body"> In web development, the CSS box model refers to how HTML
-                                elements are modeled in browser engines and how the dimensions of those HTML elements
-                                are derived from CSS properties. It is a fundamental concept for the composition of HTML
-                                webpages.CSS determines the size, position, and properties (color, background, border
-                                size, etc.) of these boxes. Every box is composed of four parts (or areas), defined by
-                                their respective edges: the content edge, padding edge, border edge, and margin edge.
+                            <div className="accordion-body"> By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates. The motivation behind the introduction of useEffect Hook is to eliminate the side-effects of using class-based components. For example, tasks like updating the DOM, fetching data from API end-points, setting up subscriptions or timers, etc can be lead to unwarranted side-effects.
                             </div>
                         </div>
                     </div>
-                    <div className="accordion-item m-3">
-                        <h2 className="accordion-header" id="headingFour">
-                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour"> What
-                                are semantic tags? </button>
-                        </h2>
-                        <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour"
-                            data-bs-parent="#accordionExample">
-                            <div className="accordion-body"> The HTML semantics refers to the tags that provide meaning to
-                                an HTML page rather than just presentation. It makes HTML more comprehensible by better
-                                defining the different sections and layout of web pages. The core characteristic of a
-                                semantic element is that it clearly communicated its meaning to both the developer and
-                                the browser. Header, Nav, Section, Article, Aside, Footer; these are examples of
-                                semantic tags. </div>
-                        </div>
-                    </div>
+
                 </div>
             </div>
 
